0865ddbf0cddb91c42b05fa542217490ce87d31b
Travis Avey <travis.avey@pm.me>
2024-06-12 20:25:40 -0400

    added method to check if list contains data
    added logic to ensure list only has one

--- dataStructures/linkedlist/linkedlist.c
+++ dataStructures/linkedlist/linkedlist.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,8 +8,17 @@ struct Node {
   uint8_t data;
 };
 
-// add a node to the list, via the data
+// add a node to the list, creates a node and appends to end of list
 void appendNode(uint8_t data, struct Node **head);
+
+// add a node to the beginning of the list
+void preprendNode(uint8_t data, struct Node **head);
+
+// delete a node. assumption that the list contains only one data object
+void deleteNode(uint8_t data, struct Node **head);
+
+// checks if data is already in the list
+bool contains(uint8_t data, struct Node *head);
 
 // helper function to print out the list
 void printList(struct Node *head);
@@ -18,9 +28,21 @@ int main(int argc, char *argv[]) {
   struct Node *head = (struct Node *)malloc(sizeof(struct Node));
   printList(head);
 
+  if (contains(10, head)) {
+    printf("10 is in list\n");
+  } else {
+    printf("10 is not in list\n");
+  }
   appendNode(8, &head);
   appendNode(12, &head);
   appendNode(9, &head);
+  printList(head);
+  preprendNode(7, &head);
+  if (contains(7, head)) {
+    printf("7 is in list\n");
+  } else {
+    printf("7 is not in list\n");
+  }
   printList(head);
 
   return 0;
@@ -28,6 +50,11 @@ int main(int argc, char *argv[]) {
 
 // add a node to the list, via the data
 void appendNode(uint8_t data, struct Node **head) {
+  if (contains(data, *head)) {
+    printf("%d is already in this list\n", data);
+    return;
+  }
+
   struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
   temp->data = data;
   temp->next = NULL;
@@ -41,6 +68,46 @@ void appendNode(uint8_t data, struct Node **head) {
       p = p->next;
     p->next = temp;
   }
+}
+
+void deleteNode(uint8_t data, struct Node **head) {
+  if (!head) {
+    printf("list is empty\n");
+    return;
+  }
+}
+
+void preprendNode(uint8_t data, struct Node **head) {
+  if (contains(data, *head)) {
+    printf("%d is already in this list\n", data);
+    return;
+  }
+
+  struct Node *node = (struct Node *)malloc(sizeof(struct Node));
+  node->data = data;
+  node->next = NULL;
+
+  struct Node *p;
+  p = *head;
+  node->next = p;
+  *head = node;
+}
+
+bool contains(uint8_t data, struct Node *head) {
+  if (!head) {
+    return false;
+  }
+
+  struct Node *p = head;
+
+  while (p) {
+    if (p->data == data) {
+      return true;
+    }
+    p = p->next;
+  }
+
+  return false;
 }
 
 // helper function to print out the list
